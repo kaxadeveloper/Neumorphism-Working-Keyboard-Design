92e05335d1a1350f633b21fbf0f00fcc3388125e
kaxadeveloper <kaxa3developer@yahoo.com>
2025-12-05 12:59:30 +0400

    app

---
+++ app.js
@@ -0,0 +1,11 @@
+const spaceBtn = document.querySelector("#space");
+const display = document.querySelector(".display");
+function disp(result) {
+    form.textarea.value = form.textarea.value + result;
+    spaceBtn.onclick = (() => {
+        form.textarea.value += " ";
+    });
+    display.ondblclick = (() => {
+        form.textarea.value = "";
+    });
+}
